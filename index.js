6d836778924487904b412b8f545f5ee7dfef86bd
Mercy Bankole <bankolemercy@gmail.com>
2022-10-28 10:12:17 +0100

    initialcommit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require('body-parser');
+
+
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const info = {
+    slackusername: "MercyB",
+    backend: true,
+    age: 27,
+    bio: "I am a beautiful lady who is a software engineer",
+  };
+
+  app.get("/get-info",(req ,res) => 
+  {
+      res.status(200).jsonp({...info})
+  }) 
+
+  
+  
+  
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`);
+  });
+
+
+
